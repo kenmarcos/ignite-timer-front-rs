3824ee069932953cf343c2e3ebf6ea0b987b0fb8
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-05-21 15:30:05 -0300

    feat: add styles to Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  CountdownSeparator,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+
+          <CountdownSeparator>:</CountdownSeparator>
+
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+};
